42efffb76394a173ef5fa432aa5b6fab3143d3bf
Justin Liman <justinliman97@gmail.com>
2022-06-20 22:44:34 -0700

    add input and buttons in html and create timer class

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("hi");
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+    start() {
+        console.log("TIMER START!");
+    }
+    pause() {
+        console.log("TIMER PAUSE!");
+    }
+    onDurationChange() {
+
+    }
+    tick() {
+
+    }
+}
+
+const durationInput = document.querySelector('#input');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
